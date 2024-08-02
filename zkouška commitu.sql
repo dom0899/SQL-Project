d2a7f2a5a266cb6963ab193d844e8185b9c24555
Dominik Dražan <drazadominik@gmail.com>
2024-08-02 16:02:47 +0200

    zkouška commitu a zjištění průměrné mzdy za určité období

--- zkouška commitu.sql
+++ zkouška commitu.sql
@@ -1,18 +1,38 @@
 -- zkouška commitu
 
-SELECT *, 
-	date_from 
-FROM czechia_price
-ORDER BY date_from DESC ; 
+
+-- zjištění průměrné mzdy za určité období 
+SELECT *
+FROM czechia_payroll cp
+WHERE value_type_code = 5958
+ORDER BY id DESC;
 
 
 
 
 
 
-SELECT 
-	cp.id,
-	cpay.id 
-FROM czechia_price cp 
-JOIN czechia_payroll cpay
-	ON cp.id = cpay.id; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
