27a47434944036206647529e9b8de3f6e8197cd4
Dominik Dražan <drazadominik@gmail.com>
2024-08-02 16:21:29 +0200

    zjištění cen potravin za určité období

--- zkouška commitu.sql
+++ zkouška commitu.sql
@@ -2,17 +2,21 @@
 
 
 -- zjištění průměrné mzdy za určité období 
+
 SELECT *
 FROM czechia_payroll cp
 WHERE value_type_code = 5958
 ORDER BY id DESC;
 
 
+-- zjištění cen potravin za určité období
 
 
 
-
-
+SELECT *
+FROM czechia_price cp
+WHERE region_code IS NOT NULL
+ORDER BY id DESC; 
 
 
 
